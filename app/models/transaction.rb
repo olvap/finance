c15c07292e61641635be26c359ebe82554b7b928
Pablo oldani <oldani.pablo@gmail.com>
2012-12-29 21:40:56 -0300

    PIMP: views

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,11 +7,17 @@ class Transaction < ActiveRecord::Base
   before_update :update_user
   after_save :change_user
   after_save :save_user
+  after_initialize :default_date
 
   validates :amount, :numericality => { :greater_than => 0 }
-  #validates :category, :presence => true
+  validates :category, :presence => true
 
   private
+
+  def default_date
+    self.date = Date.today
+  end
+
   def save_user
     user.save
   end
