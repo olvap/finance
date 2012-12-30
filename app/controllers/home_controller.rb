1107a27000add493d442d531829b7d6040d69fe0
Pablo oldani <oldani.pablo@gmail.com>
2012-12-30 11:33:05 -0300

    removes accounts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @transactions = current_user.transactions.limit(5)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ActiveRecord::Base
   attr_accessible :name, :user_id
 
   belongs_to :user
+
+  def to_s
+    name
+  end
+
 end
